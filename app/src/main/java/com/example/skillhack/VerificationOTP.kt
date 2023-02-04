06619a80bc435805c534e21cd0c82aad5722f213
sebesti0n <prashankumar0710@gmail.com>
2023-02-04 22:54:41 +0530

    Login mobile number and verification OTP page XML

---
+++ app/src/main/java/com/example/skillhack/VerificationOTP.kt
@@ -0,0 +1,11 @@
+package com.example.skillhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VerificationOTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verification_otp)
+    }
+}
