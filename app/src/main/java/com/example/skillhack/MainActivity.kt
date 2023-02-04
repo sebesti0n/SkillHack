40a2337d787316b789291f5e5a4c9ff931224442
kavikhalique <kavikhalique2@gamil.com>
2023-02-04 18:07:51 +0530

    alignment

--- app/src/main/java/com/example/skillhack/MainActivity.kt
+++ app/src/main/java/com/example/skillhack/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.skillhack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+        val x=WindowCompat.getInsetsController(window,window.decorView)
+        x.systemBarsBehavior=WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
+        //requestWindowFeature(Window.FEATURE_NO_TITLEZ
     }
 }
+
