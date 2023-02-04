50de19a1424228cc93601d46799aa4c0257f291e
sebesti0n <prashankumar0710@gmail.com>
2023-02-05 04:27:30 +0530

    Firebase Login Setup

---
+++ app/src/main/java/com/example/skillhack/checker.kt
@@ -0,0 +1,11 @@
+package com.example.skillhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class checker : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_checker)
+    }
+}
