4130eab259c7e79545c4b1b24e8183edaef2a879
kavikhalique <kavikhalique2@gamil.com>
2023-02-05 00:38:22 +0530

    splash screen animation to typewriter

--- app/src/main/java/com/example/skillhack/MainActivity.kt
+++ app/src/main/java/com/example/skillhack/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.skillhack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import android.view.Window
 import android.view.WindowInsetsController
@@ -18,6 +20,19 @@ class MainActivity : AppCompatActivity() {
         val x=WindowCompat.getInsetsController(window,window.decorView)
         x.systemBarsBehavior=WindowInsetsControllerCompat.BEHAVIOR_SHOW_BARS_BY_SWIPE
         //requestWindowFeature(Window.FEATURE_NO_TITLEZ
+        val head=findViewById<TextView>(R.id.heading)
+        head.alpha=0f
+        typerfunc()
+        head.animate().setDuration(3000).alpha(1f).withEndAction {
+            val i = Intent(this, tell_ur_name::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+
+    }
+
+    private fun typerfunc() {
         val typer=findViewById<TextView>(R.id.splash1)
         val label = "Hack a skill, earn rewards, land a job!!...."
         val stringbuilder = StringBuilder()
