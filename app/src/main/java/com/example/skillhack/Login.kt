4083c4b1a138db8f258bc5ff60248d6819a65bb1
sebesti0n <prashankumar0710@gmail.com>
2023-02-05 00:41:49 +0530

    Login mobile number and verification OTP page Activity Code

--- app/src/main/java/com/example/skillhack/Login.kt
+++ app/src/main/java/com/example/skillhack/Login.kt
@@ -1,11 +1,39 @@
 package com.example.skillhack
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class Login : AppCompatActivity() {
+    private var enternumber:EditText=findViewById(R.id.input_Mob_Num)
+    private var getotpbtn:Button=findViewById(R.id.btnGetOtp)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        getotpbtn.setOnClickListener {
+            val mobilenumber=enternumber.text
+            if(!mobilenumber.toString().trim().isEmpty()){
+                if((mobilenumber.toString().trim()).length==10){
+                    val intent=Intent(this,VerificationOTP::class.java)
+                    intent.putExtra("mobile_Number",enternumber.text.toString())
+                    startActivity(intent)
+
+                   // finish()
+                }
+                else{
+                    Toast.makeText(this,"Please enter correct number",Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(this,"Enter Mobile number",Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
--- app/src/main/java/com/example/skillhack/VerificationOTP.kt
+++ app/src/main/java/com/example/skillhack/VerificationOTP.kt
@@ -2,10 +2,35 @@ package com.example.skillhack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class VerificationOTP : AppCompatActivity() {
+    private var verifyotpbtn: Button =findViewById(R.id.btnverify)
+    private var inOTP1: EditText =findViewById(R.id.inputotp1)
+    private var inOTP2: EditText =findViewById(R.id.inputotp2)
+    private var inOTP3: EditText =findViewById(R.id.inputotp3)
+    private var inOTP4: EditText =findViewById(R.id.inputotp4)
+    private var inOTP5: EditText =findViewById(R.id.inputotp5)
+    private var inOTP6: EditText =findViewById(R.id.inputotp6)
+    private var rsndotp:TextView=findViewById(R.id.tv_resendotp)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verification_otp)
+
+        verifyotpbtn.setOnClickListener{
+            if(!inOTP1.text.toString().isEmpty()&&!inOTP2.text.toString().isEmpty()&&!inOTP3.text.toString().isEmpty()&&!inOTP4.text.toString().isEmpty()&&!inOTP5.text.toString().isEmpty()&&!inOTP6.text.toString().isEmpty())
+            {
+                Toast.makeText(this,"otp verify", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this,"Please enter correct OTP!",Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 }
